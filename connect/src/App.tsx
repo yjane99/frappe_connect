27ee7f22990a843855a3318144b6b8e9844dbbea
Yash Jane <yashjane99@gmail.com>
2023-01-20 16:13:23 +0530

    chore: add node modules

--- connect/src/App.tsx
+++ connect/src/App.tsx
@@ -10,10 +10,10 @@ function App() {
       <FrappeProvider>
         <div>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
+        <a href="https://vitejs.dev" target="blank">
           <img src="/vite.svg" className="logo" alt="Vite logo" />
         </a>
-        <a href="https://reactjs.org" target="_blank">
+        <a href="https://reactjs.org" target="blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
